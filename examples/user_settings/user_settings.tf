0a7dc2ec085c0f2e8b3a12fc92b427aaf1cd72d3
ron-ts <105591754+ron-ts@users.noreply.github.com>
2023-05-11 13:15:07 +0300

    SGA-6994 Regenerating docs with some changes (#34)
    * SGA-6994 Regenerating docs with some changes

--- examples/user_settings/user_settings.tf
+++ examples/user_settings/user_settings.tf
@@ -2,13 +2,19 @@ resource "satori_user_settings" "settings_for_user_json_path" {
   user_id  = local.user_id_to_configure_settings_on
 
   // You may define a path to a json file containing a JSON object with the attributes for the user.
-  attributes = "./attributes/user_a.json"
+  attributes = "${path.module}/attributes/user_a.json"
 }
 
 
 resource "satori_user_settings" "settings_for_user_raw_json" {
   user_id  = local.user_id_to_configure_settings_on
 
-  // You may define the attributes in a raw JSON object string.
-  attributes = "{\"name\": \"Williham\", \"age\": [3.5, 5], \"cities\": [\"Tel Aviv\", \"Lisbon\"]}"
+  // You may define the attributes in a raw JSON object using terraform's jsoncode({}).
+  attributes = jsonencode({
+    name      = "William"
+    age       = 30.5
+    cities    = ["Tel Aviv", "London", "Lisbon"]
+    is_active  = true
+    kids_ages = [1, 5, 6.5]
+  })
 }
