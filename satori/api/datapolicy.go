7e459f2c9ed545967e7af8d21127c777e3af8039
Dawid Karolewski <dawid@Dawids-MacBook-Pro.local>
2025-03-13 12:52:23 +0100

    SGA-11595 - Review

--- satori/api/datapolicy.go
+++ satori/api/datapolicy.go
@@ -3,16 +3,16 @@ package api
 const CustomPolicyDefaultPriority int = 100
 
 type AccessControl struct {
-  AccessControlEnabled        bool `json:"permissionsEnabled"`
-  CustomAccessRequestsEnabled bool `json:"customAccessRequestsEnabled"`
+	AccessControlEnabled        bool  `json:"permissionsEnabled"`
+	CustomAccessRequestsEnabled *bool `json:"customAccessRequestsEnabled"`
 }
 
 type CustomPolicy struct {
-  Priority  int    `json:"priority"`
-  RulesYaml string `json:"rulesYaml"`
-  TagsYaml  string `json:"tagsYaml"`
+	Priority  int    `json:"priority"`
+	RulesYaml string `json:"rulesYaml"`
+	TagsYaml  string `json:"tagsYaml"`
 }
 
 type SecurityPolicies struct {
-  Ids []string `json:"defaultSecurityPolicyIds"`
+	Ids []string `json:"defaultSecurityPolicyIds"`
 }
